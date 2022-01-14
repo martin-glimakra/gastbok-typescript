d5b7e27daed3a19bbcdaefaea5b3f2bccc68d99c
martin-glimakra <88892064+martin-glimakra@users.noreply.github.com>
2022-01-14 15:44:25 +0100

    post not working

---
+++ frontend/src/components/Post.tsx
@@ -0,0 +1,12 @@
+export default function Post() {
+    return (
+        <div>
+            <h2>Rubrik</h2>
+            <div>
+                <p>Inlägg</p>
+                <p>Tid</p>
+                <p>Författare</p>
+            </div>
+        </div>
+    )
+}
