be522e331cd6d5c9d6ac5d50d05107b2a5f1cfd9
martin-glimakra <88892064+martin-glimakra@users.noreply.github.com>
2022-01-18 20:49:47 +0100

    destructure

--- frontend/src/components/Post.tsx
+++ frontend/src/components/Post.tsx
@@ -1,13 +1,15 @@
 import { InterfacePost } from '../interfaces'
 
-export default function Post({
-    header, 
-    postText, 
-    author, 
-    date}: InterfacePost) {
+export default function Post(props: InterfacePost) {
+
+    const {
+        header,
+        postText, 
+        author, 
+        date} = props
 
 
-        console.log(postText, typeof postText)
+        // console.log(postText, typeof postText)
         return (
         <div>
             <h2>{header}</h2>
